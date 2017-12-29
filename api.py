0ec9c1f40352f59a60c3d25b12e30db7dd2c4b26
Toni Haka-Risku <tojuhaka@gmail.com>
2017-12-29 19:42:34 +0000

    login added to coins section

--- api.py
+++ api.py
@@ -1,19 +1,46 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from db import get_plant_data, get_forecast_data
+from flask_httpauth import HTTPBasicAuth
+from db import get_plant_data, get_forecast_data, get_crypto_data, get_spent_amount
 
 application = Flask(__name__)
 api = Api(application)
+auth = HTTPBasicAuth()
 
+users = {
+    "test": "test"
+}
 
-class Plants(Resource):
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+class Home(Resource):
     def get(self):
         result = {}
         result['forecast'] = get_forecast_data()
         result['plants'] = get_plant_data()
         return result
 
-api.add_resource(Plants, "/")
+
+class Coins(Resource):
+
+    @auth.login_required
+    def get(self):
+        profit_data = get_crypto_data()
+        amount = sum([i['amount'] for i in profit_data])
+        spent = get_spent_amount()
+
+        return {
+            'profit': ((amount - spent) / spent) * 100 ,
+            'current_amount': amount,
+            'detail': get_crypto_data()
+        }
+
+api.add_resource(Home, "/")
+api.add_resource(Coins, "/coins")
 
 
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -1,7 +1,37 @@
 import urllib
 import xmltodict
+import json
 from collections import defaultdict
 from datetime import datetime
+
+weather_symbols = {
+    "1.0": "selkeää",
+    "2.0": "puolipilvistä",
+    "21.0": "heikkoja sadekuuroja",
+    "22.0": "sadekuuroja",
+    "23.0": "voimakkaita sadekuuroja",
+    "3.0": "pilvistä",
+    "31.0": "heikkoa vesisadetta",
+    "32.0": "vesisadetta",
+    "33.0": "voimakasta vesisadetta",
+    "41.0": "heikkoja lumikuuroja",
+    "42.0": "lumikuuroja",
+    "43.0": "voimakkaita lumikuuroja",
+    "51.0": "heikkoa lumisadetta",
+    "52.0": "lumisadetta",
+    "53.0": "voimakasta lumisadetta",
+    "61.0": "ukkoskuuroja",
+    "62.0": "voimakkaita ukkoskuuroja",
+    "64.0": "voimakasta ukkosta",
+    "71.0": "heikkoja räntäkuuroja",
+    "72.0": "räntäkuuroja",
+    "73.0": "vomakkaita räntäkuuroja",
+    "81.0": "heikkoa räntäsadetta",
+    "82.0": "räntäsadetta",
+    "83.0": "voimakasta räntäsadetta",
+    "91.0": "utua",
+    "92.0": "sumua"
+}
 
 def get_plant_data() -> list:
     return [
@@ -39,8 +69,33 @@ def get_forecast_data() -> list:
     for member in members:
         name = member['BsWfs:BsWfsElement']['BsWfs:ParameterName']
         value = member['BsWfs:BsWfsElement']['BsWfs:ParameterValue']
+        if name == "Weathersymbol3":
+            value = weather_symbols[value]
         time = member['BsWfs:BsWfsElement']['BsWfs:Time']
 
         result[name].append((time, value))
-
     return result
+
+
+def get_holdings():
+    with open('/home/pi/holdings.json') as f:
+        return json.load(f)
+
+def get_spent_amount():
+    with open('/home/pi/holdings.json') as f:
+        return json.load(f)['spent_eur']
+
+def filter_crypto(data, holding_amount):
+    return {
+        "id": data['id'],
+        "amount": holding_amount * float(data['price_eur'])
+    }
+
+def get_crypto_data():
+    url = "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=100"
+    json_data = urllib.request.urlopen(url).read()
+    data = json.loads(json_data)
+
+    # filter out coin data based on holdings file
+    holdings = get_holdings()
+    return [filter_crypto(coin, holdings[coin['id']]) for coin in data if coin['id'] in holdings.keys()]
